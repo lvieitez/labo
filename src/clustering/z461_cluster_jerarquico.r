d95b66dd7b182718591833c3f57b9ffe5bf83f8e
lvieitez <89276520+lvieitez@users.noreply.github.com>
2022-05-17 21:53:06 -0300

    Update z461_cluster_jerarquico.r

--- src/clustering/z461_cluster_jerarquico.r
+++ src/clustering/z461_cluster_jerarquico.r
@@ -69,11 +69,11 @@ plot( hclust.rf )
 dev.off()
 
 
-#genero 7 clusters
+#genero 3 clusters
 h <- 20
 distintos <- 0
 
-while(  h>0  &  !( distintos >=6 & distintos <=7 ) )
+while(  h>0  &  !( distintos >=2 & distintos <=3 ) )
 {
   h <- h - 1 
   rf.cluster  <- cutree( hclust.rf, h)
